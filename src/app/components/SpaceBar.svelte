2546146ca8653e1c1ec4e6a740f32175301bcf61
Jon Staab <shtaab@gmail.com>
2026-03-13 09:11:08 -0700

    Tweak back button, hide on desktop

--- src/app/components/SpaceBar.svelte
+++ src/app/components/SpaceBar.svelte
@@ -24,10 +24,10 @@
 
 <PageBar {...props}>
   <div class="flex">
-    <Button onclick={back} class="place-self-start pr-2 sm:pr-6">
+    <Button onclick={back} class="place-self-start pr-3 md:hidden">
       <Icon icon={ArrowLeft} size={7} />
     </Button>
-    <div class="ellipsize whitespace-nowrap flex flex-grow justify-between gap-4">
+    <div class="ellipsize whitespace-nowrap flex flex-grow items-center justify-between gap-4">
       <div class="flex flex-col">
         <div class="flex gap-2 items-center">
           {@render title?.()}
